65d84cc858f8a25b73ff44e369c748f4dc052a4a
Michele <michelesalvv@gmail.com>
2021-05-22 12:38:22 +0200

    added junit dependecies

---
+++ src/it/uniroma2/dicii/isw2/jcs/paramTests/EventQueueConcurrentLoadTest.java
@@ -0,0 +1,14 @@
+package it.uniroma2.dicii.isw2.jcs.paramTests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class EventQueueConcurrentLoadTest {
+
+	@Test
+	public void test() {
+		assertEquals(true, true);
+	}
+
+}
